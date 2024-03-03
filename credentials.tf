543530cb2e8ddcb931aaf087706ddd488b1934f5
mahen27 <146904396+mahendrachoudhary27@users.noreply.github.com>
2024-03-03 22:59:07 +0530

    Update and rename credentials.tf to provider.tf

--- credentials.tf
+++
@@ -1,2 +0,0 @@
- access_key = "AKIAS3FILAAGPUQQDLU3"
- secret_key = "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"
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAS3FILAAGPUQQDLU3"
+  secret_key = "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"
+}
